1436423dfabb632a875ac9d8a5c584d2afe81c03
myrepublicid <myrepublicid@10.50.6.212>
2017-09-20 14:53:59 +0700

    add json

--- src/test/java/options/RunBddTests.java
+++ src/test/java/options/RunBddTests.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		plugin = {"pretty","html:target/destination"},
+		plugin = {"pretty","html:target/destination","json:target/destination"},
 		glue = {"stepdefs"},
 		features = {"src/test/features"})
 public class RunBddTests {}
